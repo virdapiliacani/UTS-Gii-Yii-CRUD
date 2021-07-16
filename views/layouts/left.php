43ebbf2366558166a4e4259e23bead3cd8563175
virdapiliacani <80346630+virdapiliacani@users.noreply.github.com>
2021-07-17 03:56:22 +0700

    AdminLTE

---
+++ views/layouts/left.php
@@ -0,0 +1,83 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+            </div>
+            <div class="pull-left info">
+                <p>Virdapiliacani</p>
+
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+              <span class="input-group-btn">
+                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+        <?= dmstr\widgets\Menu::widget(
+            [
+                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+                'items' => [
+                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
+                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
+					['label' => 'Home', 'url' => ['/site/index']],
+					['label' => 'Fitur', 'url' => ['/site/fitur']],
+					['label' => 'About', 'url' => ['/site/about']],
+					['label' => 'Contact', 'url' => ['/site/contact']],
+					['label' => 'Mahasiswa', 'url' => ['/mahasiswa']],
+					['label' => 'Prodi', 'url' => ['/prodi']],
+					['label' => 'Barang', 'url' => ['/barang']],
+					['label' => 'Jenis Barang', 'url' => ['/jenis']],
+					['label' => 'Supplier Barang', 'url' => ['/supplier']],
+					['label' => 'Identitas', 'url' => ['/identitas']],
+					['label' => 'Kos', 'url' => ['/kos']],
+					['label' => 'Elektronik', 'url' => ['/elektronik']],
+					['label' => 'Jenis Elektronik', 'url' => ['/jenisbarang']],
+					['label' => 'Merk', 'url' => ['/merk']],
+                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                    [
+                        'label' => 'Some tools',
+                        'icon' => 'share',
+                        'url' => '#',
+                        'items' => [
+                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
+                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
+                            [
+                                'label' => 'Level One',
+                                'icon' => 'circle-o',
+                                'url' => '#',
+                                'items' => [
+                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
+                                    [
+                                        'label' => 'Level Two',
+                                        'icon' => 'circle-o',
+                                        'url' => '#',
+                                        'items' => [
+                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
+                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ) ?>
+
+    </section>
+
+</aside>
