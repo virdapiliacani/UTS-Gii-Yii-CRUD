506a60d3fbb1c36a73211629b51cd6c2f3b94b32
virdapiliacani <80346630+virdapiliacani@users.noreply.github.com>
2021-07-17 03:26:48 +0700

    uploadfile

---
+++ views/jenis/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Jenis */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="jenis-form">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+    <?= $form->field($model, 'nama_jenis')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'keterangan')->textInput(['maxlength' => true]) ?>
+	
+	<?= $form->field($model, 'image')->FileInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
